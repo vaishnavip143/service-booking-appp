ddc9dca9653cb38dcc70e231d93657ad68ceed18
Your Name <you@example.com>
2025-08-08 03:15:38 +1245

    Initial commit

---
+++ src/main/java/com/vaishnavi/servicebook/ServiceBookBackendApplication.java
@@ -0,0 +1,13 @@
+package com.vaishnavi.servicebook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceBookBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceBookBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vaishnavi/servicebook/ServiceBookBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vaishnavi.servicebook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceBookBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
